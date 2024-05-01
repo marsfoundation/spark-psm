9bae0af1dfd761984ab1c487df57a50371ed37eb
lucas-manuel <lucasmanuel.tech@gmail.com>
2024-05-01 11:37:47 -0400

    feat: add first round of tests

--- test/L2PSM.t.sol
+++ test/L2PSM.t.sol
@@ -9,7 +9,7 @@ import { MockERC20 } from "erc20-helpers/MockERC20.sol";
 
 import { MockDsrOracle } from "./mocks/MockDsrOracle.sol";
 
-contract L2PSMTest is Test {
+contract L2PSMTestBase is Test {
 
     L2PSM public psm;
 
@@ -18,7 +18,7 @@ contract L2PSMTest is Test {
 
     MockDsrOracle public oracle;
 
-    function setUp() public {
+    function setUp() public virtual {
         sDai   = new MockERC20("sDai",  "sDai",  18);
         asset  = new MockERC20("asset", "asset", 6);
         oracle = new MockDsrOracle();
@@ -29,6 +29,21 @@ contract L2PSMTest is Test {
         psm = new L2PSM(address(sDai), address(asset), address(oracle));
     }
 
+    function _getPsmValue() internal view returns (uint256) {
+        return (sDai.balanceOf(address(psm)) * oracle.getConversionRateBinomialApprox() / 1e27)
+            + asset.balanceOf(address(psm)) * 1e12;
+    }
+
+    modifier assertPsmValueDoesNotChange {
+        uint256 beforeValue = _getPsmValue();
+        _;
+        assertEq(_getPsmValue(), beforeValue);
+    }
+
+}
+
+contract L2PSMConstructorTests is L2PSMTestBase {
+
     function test_constructor() public view {
         assertEq(address(psm.sDai()),   address(sDai));
         assertEq(address(psm.asset()),  address(asset));
@@ -37,6 +52,10 @@ contract L2PSMTest is Test {
         assertEq(psm.sDaiPrecision(),  10 ** sDai.decimals());
         assertEq(psm.assetPrecision(), 10 ** asset.decimals());
     }
+
+}
+
+contract L2PSMQuoteFunctionTests is L2PSMTestBase {
 
     function test_getBuySDaiQuote() public {
         assertEq(psm.getBuySDaiQuote(1), 0.8e12);
@@ -128,6 +147,219 @@ contract L2PSMTest is Test {
         assertEq(psm.getSellSDaiQuote(2e18), 1.6e6);
         assertEq(psm.getSellSDaiQuote(3e18), 2.4e6);
         assertEq(psm.getSellSDaiQuote(4e18), 3.2e6);
+    }
+
+}
+
+contract L2PSMBuySDaiTests is L2PSMTestBase {
+
+    address public buyer = makeAddr("buyer");
+
+    function setUp() public override {
+        super.setUp();
+
+        asset.mint(address(psm), 100e6);
+        sDai.mint(address(psm), 100e18);
+    }
+
+    function test_buySDai_amountZero() public {
+        vm.expectRevert("L2PSM/invalid-amountIn");
+        psm.buySDai(0, 0);
+    }
+
+    function test_buySDai_minAmountOutBoundary() public {
+        asset.mint(buyer, 100e6);
+
+        vm.startPrank(buyer);
+
+        asset.approve(address(psm), 100e6);
+
+        uint256 expectedAmountOut = psm.getBuySDaiQuote(100e6);
+
+        assertEq(expectedAmountOut, 80e18);
+
+        vm.expectRevert("L2PSM/invalid-amountOut");
+        psm.buySDai(100e6, 80e18 + 1);
+
+        psm.buySDai(100e6, 80e18);
+    }
+
+    function test_buySDai_insufficientApproveBoundary() public {
+        asset.mint(buyer, 100e6);
+
+        vm.startPrank(buyer);
+
+        asset.approve(address(psm), 100e6 - 1);
+
+        vm.expectRevert("SafeERC20/transfer-from-failed");
+        psm.buySDai(100e6, 80e18);
+
+        asset.approve(address(psm), 100e6);
+
+        psm.buySDai(100e6, 80e18);
+    }
+
+    function test_buySDai_insufficientUserBalanceBoundary() public {
+        asset.mint(buyer, 100e6 - 1);
+
+        vm.startPrank(buyer);
+
+        asset.approve(address(psm), 100e6);
+
+        vm.expectRevert("SafeERC20/transfer-from-failed");
+        psm.buySDai(100e6, 80e18);
+
+        asset.mint(buyer, 1);
+
+        psm.buySDai(100e6, 80e18);
+    }
+
+    function test_buySDai_insufficientPsmBalanceBoundary() public {
+        asset.mint(buyer, 125e6 + 1);
+
+        vm.startPrank(buyer);
+
+        asset.approve(address(psm), 125e6 + 1);
+
+        vm.expectRevert("SafeERC20/transfer-failed");
+        psm.buySDai(125e6 + 1, 100e18);
+
+        psm.buySDai(125e6, 100e18);
+    }
+
+    function test_buySDai() public assertPsmValueDoesNotChange {
+        asset.mint(buyer, 100e6);
+
+        vm.startPrank(buyer);
+
+        asset.approve(address(psm), 100e6);
+
+        assertEq(asset.allowance(buyer, address(psm)), 100e6);
+
+        assertEq(sDai.balanceOf(buyer),        0);
+        assertEq(sDai.balanceOf(address(psm)), 100e18);
+
+        assertEq(asset.balanceOf(buyer),        100e6);
+        assertEq(asset.balanceOf(address(psm)), 100e6);
+
+        psm.buySDai(100e6, 80e18);
+
+        assertEq(asset.allowance(buyer, address(psm)), 0);
+
+        assertEq(sDai.balanceOf(buyer),        80e18);
+        assertEq(sDai.balanceOf(address(psm)), 20e18);
+
+        assertEq(asset.balanceOf(buyer),        0);
+        assertEq(asset.balanceOf(address(psm)), 200e6);
+    }
+
+}
+
+contract L2PSMSellSDaiTests is L2PSMTestBase {
+
+    address public buyer = makeAddr("buyer");
+
+    function setUp() public override {
+        super.setUp();
+
+        asset.mint(address(psm), 100e6);
+        sDai.mint(address(psm), 100e18);
+    }
+
+    function test_sellSDai_amountZero() public {
+        vm.expectRevert("L2PSM/invalid-amountIn");
+        psm.sellSDai(0, 0);
+    }
+
+    function test_sellSDai_minAmountOutBoundary() public {
+        sDai.mint(buyer, 80e18);
+
+        vm.startPrank(buyer);
+
+        sDai.approve(address(psm), 80e18);
+
+        uint256 expectedAmountOut = psm.getSellSDaiQuote(80e18);
+
+        assertEq(expectedAmountOut, 100e6);
+
+        vm.expectRevert("L2PSM/invalid-amountOut");
+        psm.sellSDai(80e18, 100e6 + 1);
+
+        psm.sellSDai(80e18, 100e6);
+    }
+
+    function test_sellSDai_insufficientApproveBoundary() public {
+        sDai.mint(buyer, 80e18);
+
+        vm.startPrank(buyer);
+
+        sDai.approve(address(psm), 80e18 - 1);
+
+        vm.expectRevert("SafeERC20/transfer-from-failed");
+        psm.sellSDai(80e18, 100e6);
+
+        sDai.approve(address(psm), 80e18);
+
+        psm.sellSDai(80e18, 100e6);
+    }
+
+    function test_sellSDai_insufficientUserBalanceBoundary() public {
+        sDai.mint(buyer, 80e18 - 1);
+
+        vm.startPrank(buyer);
+
+        sDai.approve(address(psm), 80e18);
+
+        vm.expectRevert("SafeERC20/transfer-from-failed");
+        psm.sellSDai(80e18, 100e6);
+
+        sDai.mint(buyer, 1);
+
+        psm.sellSDai(80e18, 100e6);
+    }
+
+    function test_sellSDai_insufficientPsmBalanceBoundary() public {
+        // Prove that values yield balance boundary
+        // 0.8e12 * 1.25 = 1e12 == 1-e6 in 18 decimal precision
+        assertEq(psm.getSellSDaiQuote(80e18 + 0.8e12),     100e6 + 1);
+        assertEq(psm.getSellSDaiQuote(80e18 + 0.8e12 - 1), 100e6);
+
+        sDai.mint(buyer, 80e18 + 0.8e12);
+
+        vm.startPrank(buyer);
+
+        sDai.approve(address(psm), 80e18 + 0.8e12);
+
+        vm.expectRevert("SafeERC20/transfer-failed");
+        psm.sellSDai(80e18 + 0.8e12, 100e6);
+
+        psm.sellSDai(80e18 + 0.8e12 - 1, 100e6);
+    }
+
+    function test_sellSDai() public assertPsmValueDoesNotChange {
+        sDai.mint(buyer, 80e18);
+
+        vm.startPrank(buyer);
+
+        sDai.approve(address(psm), 80e18);
+
+        assertEq(sDai.allowance(buyer, address(psm)), 80e18);
+
+        assertEq(sDai.balanceOf(buyer),        80e18);
+        assertEq(sDai.balanceOf(address(psm)), 100e18);
+
+        assertEq(asset.balanceOf(buyer),        0);
+        assertEq(asset.balanceOf(address(psm)), 100e6);
+
+        psm.sellSDai(80e18, 100e6);
+
+        assertEq(asset.allowance(buyer, address(psm)), 0);
+
+        assertEq(sDai.balanceOf(buyer),        0);
+        assertEq(sDai.balanceOf(address(psm)), 180e18);
+
+        assertEq(asset.balanceOf(buyer),        100e6);
+        assertEq(asset.balanceOf(address(psm)), 0);
     }
 
 }
