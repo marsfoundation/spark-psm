fd1b7c75fb0f933acdce9c0d0c83a21815a76da8
lucas-manuel <lucasmanuel.tech@gmail.com>
2024-05-01 09:31:47 -0400

    feat: get first iteration of code down

--- src/L2PSM.sol
+++ src/L2PSM.sol
@@ -1,20 +1,23 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity ^0.8.13;
 
+import { IERC20 } from "erc20-helpers/interfaces/IERC20.sol";
+
+import { SafeERC20 } from "erc20-helpers/SafeERC20.sol";
+
 interface IDSROracleLike {
     function getConversionRateBinomialApprox() external view returns (uint256);
-}
-
-interface IERC20Like {
-    function decimals() external view returns (uint8);
 }
 
 // TODO: Get better name
 contract L2PSM {
 
-    address public sDai;
-    address public asset;
-    address public oracle;
+    using SafeERC20 for IERC20;
+
+    IERC20 public sDai;
+    IERC20 public asset;
+
+    IDSROracleLike public oracle;
 
     uint256 public sDaiPrecision;
     uint256 public assetPrecision;
@@ -24,19 +27,36 @@ contract L2PSM {
         require(asset_  != address(0), "L2PSM/invalid-asset");
         require(oracle_ != address(0), "L2PSM/invalid-oracle");
 
-        sDai   = sDai_;
-        asset  = asset_;
-        oracle = oracle_;
+        sDai   = IERC20(sDai_);
+        asset  = IERC20(asset_);
+        oracle = IDSROracleLike(oracle_);
 
-        sDaiPrecision  = 10 ** IERC20Like(sDai_).decimals();
-        assetPrecision = 10 ** IERC20Like(asset_).decimals();
+        sDaiPrecision  = 10 ** IERC20(sDai_).decimals();
+        assetPrecision = 10 ** IERC20(asset_).decimals();
     }
 
-    function buySDai(uint256 amountIn) external {
+    function buySDai(uint256 amountIn, uint256 minAmountOut) external {
         require(amountIn != 0, "L2PSM/invalid-amountIn");
 
-        uint256 amountOut = amountIn * sDaiPrecision / IDSROracleLike(oracle).getConversionRateBinomialApprox() / assetPrecision;
+        uint256 amountOut
+            = amountIn * sDaiPrecision / oracle.getConversionRateBinomialApprox() / assetPrecision;
 
-        require(amountOut != 0, "L2PSM/invalid-amountOut");
+        require(amountOut >= minAmountOut, "L2PSM/invalid-amountOut");
+
+        asset.safeTransferFrom(msg.sender, address(this), amountIn);
+        sDai.safeTransfer(msg.sender, amountOut);
     }
+
+    function sellSDai(uint256 amountIn, uint256 minAmountOut) external {
+        require(amountIn != 0, "L2PSM/invalid-amountIn");
+
+        uint256 amountOut
+            = amountIn * assetPrecision / oracle.getConversionRateBinomialApprox() / sDaiPrecision;
+
+        require(amountOut >= minAmountOut, "L2PSM/invalid-amountOut");
+
+        sDai.safeTransferFrom(msg.sender, address(this), amountIn);
+        asset.safeTransfer(msg.sender, amountOut);
+    }
+
 }
