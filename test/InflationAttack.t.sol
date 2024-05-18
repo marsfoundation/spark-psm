8f11df43f407314ab3ca67946713e27496a656ee
lucas-manuel <lucasmanuel.tech@gmail.com>
2024-05-18 11:18:59 -0400

    feat: test with initial burn amount passing

--- test/InflationAttack.t.sol
+++ test/InflationAttack.t.sol
@@ -53,4 +53,68 @@ contract InflationAttackTests is PSMTestBase {
         assertEq(usdc.balanceOf(firstDepositor), 15_000_000e6);
         assertEq(usdc.balanceOf(frontRunner),    15_000_000e6);
     }
+
+    function test_inflationAttack_useInitialBurnAmount_firstDepositOverflowBoundary() public {
+        psm = new PSM(address(usdc), address(sDai), address(rateProvider), 1000);
+
+        address frontRunner = makeAddr("frontRunner");
+
+        vm.startPrank(frontRunner);
+        sDai.mint(frontRunner, 800);
+        sDai.approve(address(psm), 800);
+
+        vm.expectRevert(stdError.arithmeticError);
+        psm.deposit(address(sDai), 799);
+
+        // 800 sDAI = 1000 shares
+        psm.deposit(address(sDai), 800);
+    }
+
+    function test_inflationAttack_useInitialBurnAmount() public {
+        psm = new PSM(address(usdc), address(sDai), address(rateProvider), 1000);
+
+        address firstDepositor = makeAddr("firstDepositor");
+        address frontRunner    = makeAddr("frontRunner");
+
+        // Step 1: Front runner deposits 801 sDAI to get 1 share
+
+        // 1000 shares get burned, user is left with 1
+        _deposit(frontRunner, address(sDai), 801);
+
+        assertEq(psm.shares(frontRunner), 1);
+
+        // Step 2: Front runner transfers 10m USDC to inflate the exchange rate to 1:(10m + 1)
+
+        deal(address(usdc), frontRunner, 10_000_000e6);
+
+        vm.prank(frontRunner);
+        usdc.transfer(address(psm), 10_000_000e6);
+
+        // Much less inflated exchange rate
+        assertEq(psm.convertToAssetValue(1), 9990.009990009990009991e18);
+
+        // Step 3: First depositor deposits 20 million USDC, only gets one share because rounding
+        //         error gives them 1 instead of 2 shares, worth 15m USDC
+
+        _deposit(firstDepositor, address(usdc), 20_000_000e6);
+
+        assertEq(psm.shares(firstDepositor), 2001);
+
+        // Higher amount of initial shares means lower rounding error
+        assertEq(psm.convertToAssetValue(2001), 19_996_668.887408394403731513e18);
+
+        // Step 4: Both users withdraw the max amount of funds they can
+
+        _withdraw(firstDepositor, address(usdc), type(uint256).max);
+        _withdraw(frontRunner,    address(usdc), type(uint256).max);
+
+        // Burnt shares have a claim on these
+        // TODO: Should this be an admin contract instead of address(0)?
+        assertEq(usdc.balanceOf(address(psm)), 9_993_337.774818e6);
+
+        // Front runner loses 999k USDC, first depositor loses 4k USDC
+        assertEq(usdc.balanceOf(firstDepositor), 19_996_668.887408e6);
+        assertEq(usdc.balanceOf(frontRunner),    9_993.337774e6);
+    }
+
 }
