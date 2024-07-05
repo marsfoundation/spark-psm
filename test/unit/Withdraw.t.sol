e0015a237c5ea9fed26495370921b60605daf7e4
lucas-manuel <lucasmanuel.tech@gmail.com>
2024-07-05 07:51:03 -0400

    fix: formatting

--- test/unit/Withdraw.t.sol
+++ test/unit/Withdraw.t.sol
@@ -570,7 +570,6 @@ contract PSMWithdrawTests is PSMTestBase {
         vm.prank(user1);
         amount = psm.withdraw(address(sDai), user1, type(uint256).max);
 
-
         {
             // User1s remaining shares are used
             uint256 user1SDai = (user1Shares - expectedUser1SharesBurned)
