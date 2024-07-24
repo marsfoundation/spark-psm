19ff22d898b14fbb9058bb7cd1eb13b9e9255ad5
lucas-manuel <lucasmanuel.tech@gmail.com>
2024-07-24 12:43:30 -0400

    feat: unit tests complete

--- test/unit/PreviewWIthdraw.t.sol
+++ test/unit/PreviewWIthdraw.t.sol
@@ -14,17 +14,99 @@ contract PSMPreviewWithdraw_FailureTests is PSMTestBase {
 
 }
 
+contract PSMPreviewWithdraw_ZeroAssetsTests is PSMTestBase {
+
+    function test_previewWithdraw_zeroTotalAssets() public {
+        ( uint256 shares1, uint256 assets1 ) = psm.previewWithdraw(address(dai),  1e18);
+        ( uint256 shares2, uint256 assets2 ) = psm.previewWithdraw(address(usdc), 1e6);
+        ( uint256 shares3, uint256 assets3 ) = psm.previewWithdraw(address(sDai), 1e18);
+
+        assertEq(shares1, 0);
+        assertEq(assets1, 0);
+        assertEq(shares2, 0);
+        assertEq(assets2, 0);
+        assertEq(shares3, 0);
+        assertEq(assets3, 0);
+
+        mockRateProvider.__setConversionRate(2e27);
+
+        ( shares1, assets1 ) = psm.previewWithdraw(address(dai),  1e18);
+        ( shares2, assets2 ) = psm.previewWithdraw(address(usdc), 1e6);
+        ( shares3, assets3 ) = psm.previewWithdraw(address(sDai), 1e18);
+
+        assertEq(shares1, 0);
+        assertEq(assets1, 0);
+        assertEq(shares2, 0);
+        assertEq(assets2, 0);
+        assertEq(shares3, 0);
+        assertEq(assets3, 0);
+    }
+
+}
+
 contract PSMPreviewWithdraw_SuccessTests is PSMTestBase {
 
     function setUp() public override {
         super.setUp();
-        _deposit(address(this),          address(dai),  100e18);
-        _deposit(makeAddr("other-user"), address(usdc), 10e6);
+        // Setup so that address(this) has the most shares, higher underlying balance than PSM
+        // balance of sDAI and USDC
+        _deposit(address(dai),  address(this),         100e18);
+        _deposit(address(usdc), makeAddr("usdc-user"), 10e6);
+        _deposit(address(sDai), makeAddr("sDai-user"), 1e18);
     }
 
-    function test_previewWithdraw_amountLtPsmBalance_amountLtShares() public {
-        assertEq(psm.previewWithdraw(address(dai),  1), 1);
-
-
+    function test_previewWithdraw_dai_amountLtUnderlyingBalance() public view {
+        ( uint256 shares, uint256 assets ) = psm.previewWithdraw(address(dai), 100e18 - 1);
+        assertEq(shares, 100e18 - 1);
+        assertEq(assets, 100e18 - 1);
     }
+
+    function test_previewWithdraw_dai_amountEqUnderlyingBalance() public view {
+        ( uint256 shares, uint256 assets ) = psm.previewWithdraw(address(dai), 100e18);
+        assertEq(shares, 100e18);
+        assertEq(assets, 100e18);
+    }
+
+    function test_previewWithdraw_dai_amountGtUnderlyingBalance() public view {
+        ( uint256 shares, uint256 assets ) = psm.previewWithdraw(address(dai), 100e18 + 1);
+        assertEq(shares, 100e18);
+        assertEq(assets, 100e18);
+    }
+
+    function test_previewWithdraw_usdc_amountLtUnderlyingBalanceAndLtPsmBalance() public view {
+        ( uint256 shares, uint256 assets ) = psm.previewWithdraw(address(usdc), 10e6 - 1);
+        assertEq(shares, 10e18 - 1e12);
+        assertEq(assets, 10e6 - 1);
+    }
+
+    function test_previewWithdraw_usdc_amountLtUnderlyingBalanceAndEqPsmBalance() public view {
+        ( uint256 shares, uint256 assets ) = psm.previewWithdraw(address(usdc), 10e6);
+        assertEq(shares, 10e18);
+        assertEq(assets, 10e6);
+    }
+
+    function test_previewWithdraw_usdc_amountLtUnderlyingBalanceAndGtPsmBalance() public view {
+        ( uint256 shares, uint256 assets ) = psm.previewWithdraw(address(usdc), 10e6 + 1);
+        assertEq(shares, 10e18);
+        assertEq(assets, 10e6);
+    }
+
+    function test_previewWithdraw_sdai_amountLtUnderlyingBalanceAndLtPsmBalance() public view {
+        ( uint256 shares, uint256 assets ) = psm.previewWithdraw(address(sDai), 1e18 - 1);
+        assertEq(shares, 1.25e18 - 2);
+        assertEq(assets, 1e18 - 1);
+    }
+
+    function test_previewWithdraw_sdai_amountLtUnderlyingBalanceAndEqPsmBalance() public view {
+        ( uint256 shares, uint256 assets ) = psm.previewWithdraw(address(sDai), 1e18);
+        assertEq(shares, 1.25e18);
+        assertEq(assets, 1e18);
+    }
+
+    function test_previewWithdraw_sdai_amountLtUnderlyingBalanceAndGtPsmBalance() public view {
+        ( uint256 shares, uint256 assets ) = psm.previewWithdraw(address(sDai), 1e18 + 1);
+        assertEq(shares, 1.25e18);
+        assertEq(assets, 1e18);
+    }
+
 }
