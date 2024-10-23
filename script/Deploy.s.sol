f81cf8d3cdc1519e80feacbb2c0e2af6e4dc235e
lucas-manuel <lucasmanuel.tech@gmail.com>
2024-10-23 07:48:17 -0400

    feat: initial setup working

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -5,32 +5,28 @@ import "forge-std/Script.sol";
 
 import { Base } from "lib/spark-address-registry/src/Base.sol";
 
-import { PSMDeploy } from "deploy/PSMDeploy.sol";
+import { PSM3Deploy } from "deploy/PSM3Deploy.sol";
 
-contract DeployMainnetFull is Script {
-
-    address public constant ALLOCATOR_VAULT = 0x691a6c29e9e96dd897718305427Ad5D534db16BA;
+contract DeployPSM3 is Script {
 
     function run() external {
-        vm.createSelectFork(getChain("mainnet").rpcUrl);
+        vm.createSelectFork(getChain("base").rpcUrl);
 
         console.log("Deploying PSM...");
 
         vm.startBroadcast();
 
-        address psm = PSMDeploy.deploy(({
-            owner: Base.SPARK_EXECUTOR,
-            usdc: Base.USDC,
-            usds: Base.USDS,
-            susds: Base.SUSDS,
-            rateProvider: Base.RATE_PROVIDER
-        }))
+        address psm = PSM3Deploy.deploy({
+            owner        : Base.SPARK_EXECUTOR,
+            usdc         : Base.USDC,
+            usds         : Base.USDS,
+            susds        : Base.SUSDS,
+            rateProvider : Base.SSR_AUTH_ORACLE
+        });
 
         vm.stopBroadcast();
 
-        console.log("ALMProxy   deployed at", instance.almProxy);
-        console.log("Controller deployed at", instance.controller);
-        console.log("RateLimits deployed at", instance.rateLimits);
+        console.log("PSM3 deployed at:", psm);
     }
 
 }
